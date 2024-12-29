ea684e06fc483c8f01fa9af5690e9a2a3b8459f3
Soph <sofiasawczuka@gmail.com>
2024-12-29 18:40:31 -0300

    Intento 3 de eliminar el archivo grande

--- src/components/Comodidades.jsx
+++ src/components/Comodidades.jsx
@@ -1,7 +1,7 @@
 import style from "../styles/Comodidades.module.css"
 import {AiOutlineWifi} from "react-icons/ai"
 import foto2 from "../images/Foto2.jpg"
-import esquel from "../images/atractivos-de-esquel.mp4"
+
 
 export default function Comodidades() {
     return(
@@ -11,9 +11,9 @@ export default function Comodidades() {
                 <p className={style.Description}>Dale play al video para descubrir lugares imperdibles y las mejores actividades que puedes realizar en Esquel.</p>
             </div>
             
-            <video className={style.Video} width="100%" height="65%" controls>
+           {/*  <video className={style.Video} width="100%" height="65%" controls>
                         <source src={esquel} type="video/mp4"/>
-            </video>
+            </video> */}
         </div>
     )
 }
